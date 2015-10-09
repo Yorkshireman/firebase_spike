cea416e7a26a6c8787bbec846134c35be25465a3
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-09 01:32:05 +0100

    write first proper passing test

--- test/features/firebaseSpikeFeature.js
+++ test/features/firebaseSpikeFeature.js
@@ -3,7 +3,6 @@ describe('Firebase Spike', function() {
   	browser.ignoreSynchronization = true;
     browser.get('http://localhost:8080/index.html');
 
-    expect(element(by.id('nameInput')).getText()).
-    	toEqual('5');
+    expect(element(by.id('nameInput')).isPresent()).toBe(true);
   });
 });
