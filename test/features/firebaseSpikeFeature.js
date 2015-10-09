2baed0bb5c5d7b2aa717783a838b5bb4911ae3be
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-09 01:41:55 +0100

    second feature test written and beforeEach and afterEach blocks written to ensure future test suites can be run on angular pages

--- test/features/firebaseSpikeFeature.js
+++ test/features/firebaseSpikeFeature.js
@@ -1,8 +1,19 @@
 describe('Firebase Spike', function() {
-  it('has a name input box', function() {
-  	browser.ignoreSynchronization = true;
-    browser.get('http://localhost:8080/index.html');
 
+	beforeEach(function() {
+   	browser.ignoreSynchronization = true;
+    browser.get('http://localhost:8080/index.html');
+  });
+  
+  afterEach(function() {
+  	browser.ignoreSynchronization = false;
+  })
+
+  it('has a name input box', function() {
     expect(element(by.id('nameInput')).isPresent()).toBe(true);
   });
+
+  it('has a message input box', function() {
+  	expect(element(by.id('messageInput')).isPresent()).toBe(true);
+  })
 });
