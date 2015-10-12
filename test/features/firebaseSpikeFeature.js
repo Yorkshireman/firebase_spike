54799fbf69f1ba4b4aee51465841f6865d840895
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-12 22:08:17 +0400

    test failing. Will try to install mocha chai-as-promised and see if that resolves the problem with .eventually

--- test/features/firebaseSpikeFeature.js
+++ test/features/firebaseSpikeFeature.js
@@ -16,4 +16,26 @@ describe('Firebase Spike', function() {
   it('has a message input box', function() {
   	expect(element(by.id('messageInput')).isPresent()).toBe(true);
   })
+
+  it('user can delete all messages', function() {
+    var nameBox = element(by.id('nameInput'))
+    var messageBox = element(by.id('messageInput'))
+    var messagesDiv = element(by.id('messagesDiv'))
+    var clearButton = element(by.id('clearButton'))
+    var input = $('#someInput');
+
+    clearButton.click();
+
+    nameBox.sendKeys('Andy');
+    messageBox.sendKeys('message');
+
+    input.sendKeys(protractor.Key.ENTER);
+
+    expect(messagesDiv.getText()).to.eventually.contain('Andy');
+    expect(messagesDiv.getText()).to.eventually.contain('message');
+
+    clearButton.click();
+
+    expect(messagesDiv.getText()).to.eventually.equal('');
+  })
 });
