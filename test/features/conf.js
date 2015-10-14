bdf66d1501ad2a9b67f629da895d4e30af059e64
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-14 16:29:34 +0400

    temp

--- test/features/conf.js
+++ test/features/conf.js
@@ -1,4 +1,5 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['firebaseSpikeFeature.js']
+	specs: ['firebaseSpikeFeature.js'],
+	framework: 'mocha'
 }
--- test/features/firebaseSpikeFeature.js
+++ test/features/firebaseSpikeFeature.js
@@ -1,4 +1,8 @@
 describe('Firebase Spike', function() {
+  var chai = require('chai');
+  var chaiAsPromised = require('chai-as-promised');
+  chai.use(chaiAsPromised);
+  var expect = chai.expect;
 
 	beforeEach(function() {
    	browser.ignoreSynchronization = true;
