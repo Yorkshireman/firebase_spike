e5654df2a93b342628e8718814b717f2a19ea3a1
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-17 19:56:10 +0400

    refactor and get other two tests working by moving the expect definition into isolation in the last test

--- test/features/firebaseSpikeFeature.js
+++ test/features/firebaseSpikeFeature.js
@@ -2,7 +2,10 @@ describe('Firebase Spike', function() {
   var chai = require('chai');
   var chaiAsPromised = require('chai-as-promised');
   chai.use(chaiAsPromised);
-  var expect = chai.expect;
+
+  var nameBox = element(by.id('nameInput'))
+  var messageBox = element(by.id('messageInput'))
+  var messagesDiv = element(by.id('messagesDiv'))
 
 	beforeEach(function() {
    	browser.ignoreSynchronization = true;
@@ -17,18 +20,12 @@ describe('Firebase Spike', function() {
     expect(element(by.id('nameInput')).isPresent()).toBe(true);
   });
 
-  // it('has a message input box', function() {
-  // 	expect(element(by.id('messageInput')).isPresent()).toBe(true);
-  // })
+  it('has a message input box', function() {
+  	expect(element(by.id('messageInput')).isPresent()).toBe(true);
+  })
 
-  it('user can delete all messages', function() {
-    var nameBox = element(by.id('nameInput'))
-    var messageBox = element(by.id('messageInput'))
-    var messagesDiv = element(by.id('messagesDiv'))
-    // var clearButton = element(by.id('clearButton'))
-    // var input = $('#someInput');
-
-    // clearButton.click();
+  it('user can write messages', function() {
+    var expect = chai.expect;
 
     nameBox.sendKeys('Andy');
     messageBox.sendKeys('message');
@@ -37,9 +34,5 @@ describe('Firebase Spike', function() {
 
     expect(messagesDiv.getText()).to.eventually.contain('Andy');
     expect(messagesDiv.getText()).to.eventually.contain('message');
-
-    // clearButton.click();
-
-    // expect(messagesDiv.getText()).to.eventually.equal('');
   })
 });
